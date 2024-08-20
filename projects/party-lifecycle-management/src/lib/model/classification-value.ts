7b4ab66e802b7d49aeba4005a9b4493f9d9a088d
Mladen Milić <mladen.milic@asseco-see.rs>
2024-08-20 11:42:45 +0200

    fix  lint

--- projects/party-lifecycle-management/src/lib/model/classification-value.ts
+++ projects/party-lifecycle-management/src/lib/model/classification-value.ts
@@ -1,7 +1,7 @@
 export class ClassificationValue {
-    public literal?: string;
-    public name?: string;
-    public description?: string;
-    public code?: string;
-    public externalId?: string;
-  }
+  public literal?: string;
+  public name?: string;
+  public description?: string;
+  public code?: string;
+  public externalId?: string;
+}
