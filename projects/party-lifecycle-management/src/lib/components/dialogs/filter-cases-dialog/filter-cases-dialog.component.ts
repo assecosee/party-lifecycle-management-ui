c9cece876b014c313efd76b26bba22114500a8c0
Mladen Milić <mladen.milic@asseco-see.rs>
2024-08-20 11:39:20 +0200

    fix

--- projects/party-lifecycle-management/src/lib/components/dialogs/filter-cases-dialog/filter-cases-dialog.component.ts
+++ projects/party-lifecycle-management/src/lib/components/dialogs/filter-cases-dialog/filter-cases-dialog.component.ts
@@ -1,5 +1,4 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { UntypedFormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { AseeFormControl, AseeFormGroup, AssecoFormService } from '@asseco/common-ui';
 import { forkJoin, Observable, of } from 'rxjs';
