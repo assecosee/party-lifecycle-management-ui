b4865c1a2da975735a48a413d798cf096a21cd9f
Almir Hadžimuhović <almir.hadzimuhovic@asseco-see.rs>
2024-09-05 11:35:26 +0200

    Remove logs

--- projects/party-lifecycle-management/src/lib/utils/directives/uppercase-directive.ts
+++ projects/party-lifecycle-management/src/lib/utils/directives/uppercase-directive.ts
@@ -7,7 +7,6 @@ import { Directive, HostListener } from '@angular/core';
 export class UppercaseDirective {
   @HostListener('input', ['$event']) onInput(event: KeyboardEvent) {
     const input = event.target as HTMLInputElement;
-    console.log(input.value);
     input.value = input.value.toUpperCase();
   }
 }
