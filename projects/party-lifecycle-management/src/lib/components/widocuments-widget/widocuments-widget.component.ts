5a8b7c8f1d340c0fa11cdf616587ddca4bfa8c66
StefanASSE <stojanovic.stefan@asseco-see.rs>
2024-09-24 09:30:14 +0200

    No information on widget

--- projects/party-lifecycle-management/src/lib/components/widocuments-widget/widocuments-widget.component.ts
+++ projects/party-lifecycle-management/src/lib/components/widocuments-widget/widocuments-widget.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import { BaseWidget } from '@asseco/components-ui';
+import { L10N_LOCALE, L10nLocale } from 'angular-l10n';
 
 @Component({
   selector: 'lib-widocuments-widget',
@@ -9,6 +10,13 @@ import { BaseWidget } from '@asseco/components-ui';
 export class WIDocumentsWidgetComponent extends BaseWidget implements OnInit{
   override title: string;
   override configuration?: any;
+  public locale: L10nLocale;
+  constructor(
+    private injector: Injector
+  ){
+    super();
+    this.locale = injector.get(L10N_LOCALE);
+  }
   ngOnInit(): void {
     this.title = 'Documents';
   }
