47c516206da70bb184ec3601b74b97acf7ab5301
Mladen Milić <mladen.milic@asseco-see.rs>
2024-10-09 09:56:44 +0200

    fix

--- projects/party-lifecycle-management/src/lib/components/task-widget/task-widget.component.ts
+++ projects/party-lifecycle-management/src/lib/components/task-widget/task-widget.component.ts
@@ -5,7 +5,8 @@ import { MaterialTaskListComponent } from '@asseco/task-inbox';
 import { L10N_LOCALE, L10nLocale } from 'angular-l10n';
 import { DashboardDataStoreService } from '../../services/dashboard-data-store.service';
 import { Case } from '../../model/case';
-import { BpmTasksHttpClient } from '@asseco/common-ui';
+import { BpmTasksHttpClient, BrokerFacade, BrokerMessage } from '@asseco/common-ui';
+import { debounceTime, Subscription } from 'rxjs';
 
 @Component({
   selector: 'lib-task-widget',
@@ -30,16 +31,24 @@ export class TaskWidgetComponent extends BaseWidget implements OnInit{
     protected cdr: ChangeDetectorRef,
     protected dashboardDataService: DashboardDataStoreService,
     protected taskService: BpmTasksHttpClient,
-    private injector: Injector
+    private injector: Injector,
+    private brokerFacade: BrokerFacade
   ){
     super();
     this.locale = injector.get(L10N_LOCALE);
   }
+  private processStateSubscription: Subscription;
   ngOnInit(): void {
     this.title = 'Tasks';
     this.dashboardDataService.subjects['case'].subscribe((dataCase: Case) => {
       this.caseNumber = dataCase.id;
     });
+    this.processStateSubscription = this.brokerFacade.subscribe('bpm',
+      'businessKey = \'' + this.caseNumber + '\' and kind = \'process-state-event\'')
+      .pipe(debounceTime(500))
+      .subscribe((message: BrokerMessage) => {
+        this.activeTasksContainer.resetLoading(true);
+      });
   }
 
   public tabChange(event: MatTabChangeEvent) {
